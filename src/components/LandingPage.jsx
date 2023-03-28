ef42ff3fd3aa920be8e1ccdc3f8f18650b338b77
esther <esther.githinji@student.moringaschool.com>
2023-03-28 15:03:31 +0300

    modification to landingpage2

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,25 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Landingpage.css'
 
 export default function LandingPage() {
   return (
-    <div style={{ 
-      backgroundImage: `url("https://asset.nasen.org.uk/styles/690_auto/public/media/2020-11/generic%20book%20open.jpg?itok=mS2HwCG0")`,
-      height:'900px',
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-      fontWeight: "bold",
-      justifyContent: "center"
-      
-
-    }}>
-      <div style={{
-        width: "50%",
-        textAlign: "center"
-      }}>
-        <h1>WORDLINE</h1>
-        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+    <div className='Landing-container'>
+      <div className="landing__container">
+        <div className="headings">
+          <h1>💫 Welcome to Wordline 💫</h1>
+          <br/>
+          <h2>Reality doesn’t always give us the life that we desire,<br/> but we can always find what we desire between the pages of books.</h2>
+        </div>
+        <div className="landing-details">
+          <img src="https://www.apple.com/newsroom/images/product/apple-books/apple-books_reading-now_06122018_big.jpg.large.jpg"/>
+          <br/>
+            <button type='button' href="#">Sign UP</button>
+            <button type='button' href="#">Log In</button>
       </div>
-
+      </div>
     </div>
   )
 }
