8155d889214e69e9e252d529fbe1991cc537b843
patrick kisyula <pmusyimi42@gmail.com>
2023-04-02 15:29:05 +0300

    finalised navbar and landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -9,10 +9,10 @@ export default function LandingPage() {
         <div className="headings">
           <h1>💫 Welcome to Wordline 💫</h1>
           <br/>
-          <h2>Reality doesn’t always give us the life that we desire,<br/> but we can always find what we desire between the pages of books.</h2>
+          <h2>Reality doesn't always give us the life that we desire,<br/> but we can always find what we desire between the pages of books.</h2>
         </div>
         <div className="landing-details">
-          <img src="https://www.apple.com/newsroom/images/product/apple-books/apple-books_reading-now_06122018_big.jpg.large.jpg"/>
+          <img src="https://media.istockphoto.com/id/943117712/vector/online-library-and-knowledge.jpg?s=612x612&w=0&k=20&c=YuTuZLTAqtxkEHTRBqKTbSG4ss8VU-2mXRrx5dK7z3s=" alt=''/>
           <br/>
           <button>
             <Link type='button' to="/signup">Sign UP</Link>
@@ -20,8 +20,11 @@ export default function LandingPage() {
           <button>
             <Link type='button' to="/login">Log In</Link>
           </button>
+        </div>
       </div>
-      </div>
+      <section>
+        
+      </section>
     </div>
   )
 }
