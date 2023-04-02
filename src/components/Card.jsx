bde0e489522c51c94e9d12eccf7335b9022e221d
Felix <felix.irungu@student.moringaschool.com>
2023-04-02 16:42:18 +0300

    Display Updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,74 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Popup from 'reactjs-popup';
+import ShowBook from './ShowBook';
 
 export default function Card() {
+  const [books, setBooks] = useState([]);
+  const [selectedBook, setSelectedBook] = useState(null);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const res = await fetch('/books');
+        const data = await res.json();
+        setBooks(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchBooks();
+  }, []);
+
+  const handleBookSelect = (book) => {
+    setSelectedBook(book);
+  };
+
   return (
-    <div>
-        <div className="card" style={{width: "18rem"}}>
-        <img src="https://nuriakenya.com/wp-content/uploads/2022/03/Ugly-Love-nuriakenya.jpg" className="card-img-top" alt="..."/>
-        <div className="card-body">
-            <h5 className="card-title">Ugly Love</h5>
-            <small>Colleen Hoover</small>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
+    <div className='card-container'>
+      {books.map((book) => (
+        <div className='cards' style={{ width: '18rem' }} key={book.id}>
+          <img src={book.cover} className='card-img-top' alt='...' />
+          <div className='card-body'>
+            <h5 className='card-title' style={{ fontWeight: 'bold' }}>
+              {book.name}
+            </h5>
+            <div className='span'>
+              <span>Written by: {book.author.name}</span>
+            </div>
+            <Popup trigger={<button className="btn btn btn-info"> View Details </button>} modal nested>
+            {close => (
+              <div style={{backgroundColor: "aquamarine", padding: "5%", borderRadius: "20px", border: "solid"}}>
+                  <div className="modal" style={{}}>
+                      <button className="close" onClick={close}>
+                          &times;
+                      </button>
+                  </div>
+                  <div className="header" style={{
+                    fontSize: "2em",
+                    borderBottom: "1px solid white",
+                    width: "60%",
+                    }}>{book.title}</div>
+                  <div className="content" style={{width: "60%",
+                    padding: "10px 5px",
+                    height: "30rem",
+                    display: "grid",
+                    placeItems: "center",
+                    fontSize: "1.5rem",
+                  }}>{book.description}</div>
+                  <div className="reviews">
+                     <h2 style={{fontWeight: "bold"}}>Reviews</h2>
+                  </div>
+                  <div className="reviews" style={{fontSize: "2em", borderBottom: "1px solid white"}}>{book.reviews.comment}</div>
+                  <div className="actions" style={{ margin: "auto", textAlign: "center"}}>
+                  <button className="button" onClick={() => {close()}}>Close</button>
+                  </div>
+              </div>
+            )}
+            </Popup>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
